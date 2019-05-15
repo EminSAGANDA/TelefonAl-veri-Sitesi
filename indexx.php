0f738e2b4b513fd3d0454c2ed2a701a6f6651c6b
EminSAGANDA <EminSAGANDA>
2019-05-15 22:04:59 +0300

    yeni

---
+++ indexx.php
@@ -0,0 +1,19 @@
+<?php  include'header.php'; ?>
+<?php  include'sidebar.php'; ?>
+
+ <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">Admin Panel</h1>
+                        <h1 class="page-subhead-line">Sitenizi yönetmek için sol taraftaki menüleri kullanabilirsiniz. </h1>
+
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                
+
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+<?php  include'footer.php'; ?>
